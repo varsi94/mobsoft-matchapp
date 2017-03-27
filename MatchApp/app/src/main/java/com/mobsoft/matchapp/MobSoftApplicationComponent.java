005993bcdad78f6ce124dc2eb6b32e5dd8744471
varsi94 <varsi.marci@gmail.com>
2017-03-27 11:28:39 +0200

    updated component

--- MatchApp/app/src/main/java/com/mobsoft/matchapp/MobSoftApplicationComponent.java
+++ MatchApp/app/src/main/java/com/mobsoft/matchapp/MobSoftApplicationComponent.java
@@ -4,6 +4,7 @@ import com.mobsoft.matchapp.ui.UiModule;
 import com.mobsoft.matchapp.ui.details.DetailsActivity;
 import com.mobsoft.matchapp.ui.editor.EditorActivity;
 import com.mobsoft.matchapp.ui.main.MainActivity;
+import com.mobsoft.matchapp.ui.matchlist.MatchListActivity;
 import com.mobsoft.matchapp.ui.standings.StandingsActivity;
 
 import javax.inject.Singleton;
@@ -21,4 +22,5 @@ public interface MobSoftApplicationComponent {
     void inject(StandingsActivity standingsActivity);
     void inject(DetailsActivity detailsActivity);
     void inject(EditorActivity editorActivity);
+    void inject(MatchListActivity matchListActivity);
 }
